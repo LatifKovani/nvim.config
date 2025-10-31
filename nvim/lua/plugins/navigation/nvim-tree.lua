3d9bb4178e0ba09134f3652285ccd9acda4ae610
LatifKovani <latif.kovani.st@uni.gjilan.net>
2025-10-31 22:28:46 +0100

    lsp update

---
+++ nvim/lua/plugins/navigation/nvim-tree.lua
@@ -0,0 +1,45 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = "nvim-tree/nvim-web-devicons",
+
+  -- move here ⬇️
+  init = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+  end,
+
+  config = function()
+    local nvimtree = require "nvim-tree"
+
+    nvimtree.setup {
+      view = { width = 35, relativenumber = true },
+      renderer = {
+        indent_markers = { enable = true },
+        icons = {
+          glyphs = {
+            folder = {
+              arrow_closed = "",
+              arrow_open = "",
+            },
+          },
+        },
+      },
+      actions = {
+        open_file = {
+          window_picker = { enable = false },
+        },
+      },
+      filters = {
+        custom = { ".DS_Store" },
+      },
+      git = { ignore = false },
+    }
+
+    -- keymaps
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle explorer on current file" })
+    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse explorer" })
+    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh explorer" })
+  end,
+}
