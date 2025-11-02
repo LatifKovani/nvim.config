9df0bbc7a0d6d40c9a5775872f650e42271be6c6
LatifKovani <latif.kovani.st@uni.gjilan.net>
2025-11-02 10:19:44 +0100

    barbecue-nvim

--- nvim/lua/latif/core/options.lua
+++ nvim/lua/latif/core/options.lua
@@ -4,6 +4,7 @@ local opt = vim.opt -- for conciseness
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+vim.opt.linespace = 6 -- adds 2 pixels between lines
 
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
--- nvim/lua/latif/plugins/which-key.lua
+++ nvim/lua/latif/plugins/which-key.lua
@@ -1,13 +1,37 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		icons = {
+			breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+			separator = "➜", -- symbol used between a key and it's label
+			group = "", -- symbol prepended to a group
+		},
+		preset = "classic",
+		win = {
+			border = vim.g.border_enabled and "rounded" or "none",
+			no_overlap = false,
+		},
+		delay = function()
+			return 0
+		end,
+	},
+	config = function(_, opts)
+		require("which-key").setup(opts)
+		require("which-key").add({
+			{
+				{ "<leader>s", group = "Sessions", icon = "󰔚" },
+				{ "<leader>e", group = "FileExplorer", icon = "󰮗" },
+				{ "<leader>f", group = "Find", icon = "" },
+				{ "<leader>h", group = "LSP", icon = "" },
+				{ "<leader>r", group = "Neovim", icon = "" },
+				{ "<leader>o", group = "Options", icon = "" },
+				{ "<leader>t", group = "Terminal", icon = "" },
+				{ "<leader>x", group = "Diagnostics", icon = "" },
+				{ "<leader>k", group = "Buffers", icon = "" },
+				{ "<leader>l", group = "Git", icon = "" },
+				{ "<leader>G", group = "Git", icon = "" },
+			},
+		})
+	end,
 }
